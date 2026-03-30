9313915f40c61d95c7ab165f2ec94d73668c42ca
telfordm1sph <mis.trainingmaterials@gmail.com>
2026-03-30 13:11:00 +0800

    fix: api to route migration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
+        // api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
@@ -48,10 +48,11 @@ return Application::configure(basePath: dirname(__DIR__))
         });
     })
     ->withMiddleware(function (Middleware $middleware): void {
-            $middleware->encryptCookies(except: [
+        $middleware->prepend(StartSession::class);
+
+        $middleware->encryptCookies(except: [
             'sso_token',
         ]);
-        $middleware->append(StartSession::class);
 
         $middleware->web([
             \App\Http\Middleware\HandleInertiaRequests::class,
